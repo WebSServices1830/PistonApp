b250002efd2fcc532c6f0bb1021d0f4aee98ca39
Juanap12 <juanpablocj@gmail.com>
2018-10-18 11:42:25 -0500

    Se arreglaron cosas importantes del registro, tanto en front como en back

--- Front-end/AndroidClient_PistonApp/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/UsuarioPck/CrearUsuarioView.java
+++ Front-end/AndroidClient_PistonApp/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/UsuarioPck/CrearUsuarioView.java
@@ -1,13 +1,29 @@
 package co.edu.javeriana.sebastianmesa.conexmongo.UsuarioPck;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +34,9 @@ import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -25,19 +44,50 @@ import co.edu.javeriana.sebastianmesa.conexmongo.R;
 
 public class CrearUsuarioView extends AppCompatActivity {
 
+    private Activity activityContext = this;
 
-    private EditText nombreUsuario, contra, edad, descripcion, foto, bolsillo;
+    private EditText nombreUsuario, contra, foto, fechaNacimiento;
+    private ImageButton btn_calendario, btn_seleccionarImagen, btn_tomarFoto;
+    private ImageView previewFoto;
     private CheckBox admin;
     private Button agregarP, consultaP, accionesPiloto;
+
     private String resultado="";
     private WebMet_AgregarUsuario wm_agregarPiloto = null;
     private TextView campo = null;
+
+    private final static int STORAGE_PERMISSION = 1;
+    private final static int CAMERA_PERMISSION = 2;
+
+    static final int IMAGE_PICKER_REQUEST = 10;
+    static final int IMAGE_CAPTURE_REQUEST = 20;
+
+    //Calendario para obtener fecha & hora
+    private final Calendar calendario = Calendar.getInstance();
+
+    private static final String CERO = "0";
+    private static final String BARRA = "/";
+
+    //Variables para obtener la fecha
+    final int mes = calendario.get(Calendar.MONTH);
+    final int dia = calendario.get(Calendar.DAY_OF_MONTH);
+    final int anio = calendario.get(Calendar.YEAR);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_usuario_view);
+
+        nombreUsuario = (EditText) findViewById(R.id.nomUsuario);
+        contra = (EditText) findViewById(R.id.passUsuario);
+        foto = (EditText) findViewById(R.id.fotoUsuario);
+        fechaNacimiento = findViewById(R.id.editText_fechaNacimiento);
+        btn_calendario = findViewById(R.id.imageButton_calendario);
+        admin = (CheckBox) findViewById(R.id.adminUsuario);
+        btn_seleccionarImagen = findViewById(R.id.imageButton_seleccionarImagen);
+        btn_tomarFoto = findViewById(R.id.imageButton_tomarFoto);
+        previewFoto = findViewById(R.id.imageView_fotoPerfil);
 
         agregarP =(Button) findViewById(R.id.agregarUsuario);
         agregarP.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +98,29 @@ public class CrearUsuarioView extends AppCompatActivity {
                 wm_agregarPiloto.execute();
             }
         });
+
+        btn_calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerFecha();
+            }
+        });
+
+        btn_seleccionarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission(activityContext, Manifest.permission.READ_EXTERNAL_STORAGE,"Se necesita acceso al almacenamiento",STORAGE_PERMISSION);
+            }
+        });
+
+        btn_tomarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission(activityContext, Manifest.permission.CAMERA, "Se necesita acceso a la cámara", CAMERA_PERMISSION);
+            }
+        });
+
+
 
     }
 
@@ -63,18 +136,17 @@ public class CrearUsuarioView extends AppCompatActivity {
 
             SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
 
-            nombreUsuario = (EditText) findViewById(R.id.nomUsuario);
-            contra = (EditText) findViewById(R.id.passUsuario);
-            foto = (EditText) findViewById(R.id.fotoUsuario);
+            Calendar fechaNacimiento = new GregorianCalendar();
+            fechaNacimiento.set(anio,mes,dia);
 
-            admin = (CheckBox) findViewById(R.id.adminUsuario);
+            Log.i("Fecha:",calendario.getTime().toString());
 
 
             request.addProperty("nombreUsuario", nombreUsuario.getText().toString());
             request.addProperty("contrasenia", contra.getText().toString());
-            request.addProperty("fechaNacimiento", new Date());
+            request.addProperty("fechaNacimiento", calendario.getTime());
             request.addProperty("foto", foto.getText().toString());
-            request.addProperty("admin", admin.isSelected());
+            request.addProperty("admin", admin.isChecked());
 
 
             SoapSerializationEnvelope envelope =  new SoapSerializationEnvelope(SoapEnvelope.VER11);
@@ -118,6 +190,110 @@ public class CrearUsuarioView extends AppCompatActivity {
         @Override
         protected void onCancelled() {
             Toast.makeText(getApplicationContext(), 	"Error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void obtenerFecha(){
+        DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendario.set(Calendar.YEAR,year);
+                calendario.set(Calendar.MONTH,month);
+                calendario.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
+                final int mesActual = month + 1;
+                //Formateo el día obtenido: antepone el 0 si son menores de 10
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                //Formateo el mes obtenido: antepone el 0 si son menores de 10
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                //Muestro la fecha con el formato deseado
+                fechaNacimiento.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
+
+
+            }
+            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+            /**
+             *También puede cargar los valores que usted desee
+             */
+        },anio, mes, dia);
+        //Muestro el widget
+        recogerFecha.show();
+
+    }
+
+    private void requestPermission(Activity context, String permission, String explanation, int requestId ){
+        if (ContextCompat.checkSelfPermission(context,permission)!= PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?   
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context,permission)) {
+                Toast.makeText(context, explanation, Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permission}, requestId);
+        }else{
+            switch (requestId){
+                case STORAGE_PERMISSION : {
+                    Intent pickImage = new Intent(Intent.ACTION_PICK);
+                    pickImage.setType("image/*");
+                    startActivityForResult(pickImage,IMAGE_PICKER_REQUEST);
+                    break;
+
+                }
+                case CAMERA_PERMISSION : {
+                    takePicture();
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case STORAGE_PERMISSION : {
+                Intent pickImage = new Intent(Intent.ACTION_PICK);
+                pickImage.setType("image/*");
+                startActivityForResult(pickImage,IMAGE_PICKER_REQUEST);
+                break;
+
+            }
+            case CAMERA_PERMISSION : {
+                takePicture();
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case IMAGE_PICKER_REQUEST:
+                if(resultCode == RESULT_OK){
+                    try{
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        previewFoto.setImageBitmap(selectedImage);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case IMAGE_CAPTURE_REQUEST:
+                if(resultCode == RESULT_OK){
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap  = (Bitmap) extras.get("data");
+                    previewFoto.setImageBitmap(imageBitmap);
+                }
+        }
+    }
+
+    private void takePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST);
         }
     }
 }

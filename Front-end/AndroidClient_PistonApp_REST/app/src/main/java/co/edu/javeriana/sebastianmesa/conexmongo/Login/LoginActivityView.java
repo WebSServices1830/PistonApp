4f2dd6b9d4e284b814634adc9c2aa2d001420af7
Sebastian Mesa <semeguze@hotmail.com>
2018-11-17 12:33:44 -0500

    Cambios gradle
    Convertir XML a JSON para manejar respuesta del servidor

--- Front-end/AndroidClient_PistonApp_REST/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/Login/LoginActivityView.java
+++ Front-end/AndroidClient_PistonApp_REST/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/Login/LoginActivityView.java
@@ -21,26 +21,16 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.MarshalDate;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import butterknife.ButterKnife;
 import co.edu.javeriana.sebastianmesa.conexmongo.AdminMainActivity;
@@ -49,22 +39,17 @@ import co.edu.javeriana.sebastianmesa.conexmongo.ObjetosNegocio.Usuario;
 import co.edu.javeriana.sebastianmesa.conexmongo.R;
 import co.edu.javeriana.sebastianmesa.conexmongo.UserMenuActivity;
 import co.edu.javeriana.sebastianmesa.conexmongo.UsuarioPck.CrearUsuarioView;
+import fr.arnaudguyon.xmltojsonlib.XmlToJson;
 
 public class LoginActivityView extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
     private TextView _signupLink, _emailText, _passwordText;
     private Button _loginButton;
-    private WebMet_ValidarLogin wm_validarLogin = null;
 
     private LinearLayout ll;
-
-    Usuario usuarioLogeado;
 
 
     @Override
@@ -72,28 +57,6 @@ public class LoginActivityView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_view);
         ButterKnife.bind(this);
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    getUsuario(user);
-                    if(!usuarioLogeado.isAdmin()) {
-                        startActivity(new Intent(LoginActivityView.this, UserMenuActivity.class));
-                    } else {
-                        startActivity(new Intent(LoginActivityView.this, AdminMainActivity.class));
-                    }
-                } else {
-                // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
@@ -147,48 +110,56 @@ public class LoginActivityView extends AppCompatActivity {
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG,
-                                "signInWithEmail:onComplete:" + task.isSuccessful());
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail:failed", task.getException());
-                            _emailText.setText("");
-                            _passwordText.setText("");
-                        }
-                    }
-                });
 
-        /*wm_validarLogin = new WebMet_ValidarLogin();
-        wm_validarLogin.execute();*/
+        //Lógica validar usuario!
+//        wm_validarLogin = new WebMet_ValidarLogin();
+//        wm_validarLogin.execute();
+
+        sendAndRequestResponse(email, password);
+
 
         new android.os.Handler().postDelayed(
-            new Runnable() {
-                public void run() {
-                    // On complete call either onLoginSuccess or onLoginFailed
-                    onLoginSuccess();
-                    // onLoginFailed();
-                    progressDialog.dismiss();
-                }
-            }, 3000);
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
+
+    private void sendAndRequestResponse(String nombre, String pass) {
+
+        RequestQueue mRequestQueue;
+        StringRequest mStringRequest;
+        String url = "http://10.0.2.2:8080/myapp/PistonApp/usuarios/";
+
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url + nombre, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                XmlToJson xmlToJson = new XmlToJson.Builder(response).build();
+
+                JSONObject jsonObject = xmlToJson.toJson();
+
+                Log.i("intentoLogin",jsonObject.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i("intentoLogin","Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 
 
@@ -247,6 +218,8 @@ public class LoginActivityView extends AppCompatActivity {
         return valid;
     }
 
+
+
     public String computeHash(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         digest.reset();
@@ -254,135 +227,10 @@ public class LoginActivityView extends AppCompatActivity {
         byte[] byteData = digest.digest(input.getBytes("UTF-8"));
         StringBuffer sb = new StringBuffer();
 
-        for (int i = 0; i < byteData.length; i++) {
+        for (int i = 0; i < byteData.length; i++){
             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
         }
         return sb.toString();
     }
-
-    public void getUsuario(FirebaseUser user){
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://10.0.2.2:8080/myapp/PistonApp/";
-        String path = "usuarios/"+user.getEmail();
-        StringRequest req = new StringRequest(Request.Method.GET, url+path,
-                new Response.Listener() {
-                    public void onResponse(Object response) {
-                        usuarioLogeado = (Usuario)response;
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i("TAG", "Error handling rest invocation"+error.getCause());
-                    }
-                }
-        );
-        queue.add(req);
-    }
-
-    private class WebMet_ValidarLogin extends AsyncTask<Void, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-            //WebService - Opciones
-            final String NAMESPACE = "http://webservice.javeriana.co/";
-            final String URL = "http://10.0.2.2:8080/WS/autenticacion?wsdl";
-            final String METHOD_NAME = "validarLogin";
-            final String SOAP_ACTION = "http://webservice.javeriana.co/validarLogin";
-
-            String user = _emailText.getText().toString();
-            String password = _passwordText.getText().toString();
-
-            String contra_hash = null;
-            try {
-                contra_hash = computeHash(password);
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-                Log.i("Error: ", e.getMessage());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                Log.i("Error: ", e.getMessage());
-            }
-
-            if (contra_hash == null) {
-                return false;
-            }
-
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
-            request.addProperty("nombreUsuario", user);
-            request.addProperty("contrasenia", contra_hash);
-
-            Log.i("Login", contra_hash);
-
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.setOutputSoapObject(request);
-
-            HttpTransportSE ht = new HttpTransportSE(URL);
-            try {
-
-                //campoRespuesta = (TextView) findViewById(R.id.respuestaConsulta);
-                //campoRespuesta.setText("");
-
-                ht.call(SOAP_ACTION, envelope);
-                SoapObject response = (SoapObject) envelope.getResponse();
-
-                if (response != null) {
-
-                    String nombreUsuario = response.getPrimitivePropertyAsString("nombreUsuario");
-                    String contrasenia = response.getPrimitivePropertyAsString("contra");
-                    Date fechaNacimiento = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(response.getPrimitivePropertyAsString("fechaNacimiento"));
-                    String urlFoto = response.getPrimitivePropertyAsString("urlFoto");
-                    boolean admin = Boolean.parseBoolean(response.getPrimitivePropertyAsString("admin"));
-                    double bolsillo = Double.parseDouble(response.getPrimitivePropertyAsString("bolsillo"));
-
-                    ManagerUsuario.usuario = new Usuario();
-
-                    ManagerUsuario.usuario.setNombreUsuario(nombreUsuario);
-                    ManagerUsuario.usuario.setContra(contrasenia);
-                    ManagerUsuario.usuario.setFechaNacimiento(fechaNacimiento);
-                    ManagerUsuario.usuario.setUrlFoto(urlFoto);
-                    ManagerUsuario.usuario.setAdmin(admin);
-                    ManagerUsuario.usuario.setBolsillo(bolsillo);
-
-                    if (ManagerUsuario.usuario.isAdmin()) {
-                        startActivity(new Intent(getBaseContext(), AdminMainActivity.class));
-                    } else {
-                        startActivity(new Intent(getBaseContext(), UserMenuActivity.class));
-                    }
-
-                    return true;
-                }
-
-
-            } catch (Exception e) {
-                Log.i("Error", e.getMessage());
-                //Toast.makeText(getApplicationContext(), "No encontrado", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(getBaseContext(), LoginActivityView.class));
-                e.printStackTrace();
-
-            }
-
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            if (success == false) {
-                startActivity(new Intent(getBaseContext(), LoginActivityView.class));
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-            } else {
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            startActivity(new Intent(getBaseContext(), LoginActivityView.class));
-            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-        }
-    }
-
 
 }

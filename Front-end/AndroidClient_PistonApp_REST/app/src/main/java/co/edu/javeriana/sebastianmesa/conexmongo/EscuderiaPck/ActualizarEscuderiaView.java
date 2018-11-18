8b49b778751cb4fbe8e361fd1e6d21671ffb4ae3
Álvaro Pongutá <alvaro2102@hotmail.com>
2018-11-18 14:07:19 -0500

    actualizar escuderia

--- Front-end/AndroidClient_PistonApp_REST/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/EscuderiaPck/ActualizarEscuderiaView.java
+++ Front-end/AndroidClient_PistonApp_REST/app/src/main/java/co/edu/javeriana/sebastianmesa/conexmongo/EscuderiaPck/ActualizarEscuderiaView.java
@@ -10,18 +10,43 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import co.edu.javeriana.sebastianmesa.conexmongo.ObjetosNegocio.Escuderia;
 import co.edu.javeriana.sebastianmesa.conexmongo.R;
 
 public class ActualizarEscuderiaView extends AppCompatActivity {
-
+    private final static String TAG = "Log_ActualizarEscuderia";
+    private  List<Escuderia> escuderias = new ArrayList<>();
     private EditText nombre, lugarBase, jefeEquipo, jefeTecnico, chasis, fotoEscudo_ref, cant_vecesEnPodio, cant_TitulosCampeonato;
     private Button updateEscuderia;
     private String resultado="";
@@ -119,6 +144,159 @@ public class ActualizarEscuderiaView extends AppCompatActivity {
         protected void onCancelled() {
             Toast.makeText(getApplicationContext(), 	"Error", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void consumeRESTVolleyActualizarEscuderia(String id){
+        nombre = (EditText) findViewById(R.id.nomEscuderia);
+        lugarBase = (EditText) findViewById(R.id.nomBase);
+        jefeEquipo = (EditText) findViewById(R.id.nomJEquipo);
+        jefeTecnico = (EditText) findViewById(R.id.nomJTecnico);
+        chasis = (EditText) findViewById(R.id.nomChasis);
+        fotoEscudo_ref = (EditText) findViewById(R.id.refImagen);
+        cant_vecesEnPodio = (EditText) findViewById(R.id.numPodios);
+        cant_TitulosCampeonato = (EditText) findViewById(R.id.numTitulos);
+
+        Escuderia escuderia = new Escuderia(nombre.getText().toString(), lugarBase.getText().toString(),
+                jefeTecnico.getText().toString(), jefeEquipo.getText().toString(), chasis.getText().toString(),
+                Integer.parseInt(cant_vecesEnPodio.getText().toString()),
+                Integer.parseInt(cant_TitulosCampeonato.getText().toString()), fotoEscudo_ref.getText().toString());
+
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
+        String result = gson.toJson(escuderia);
+
+        JSONObject js_2 = null;
+        try {
+            js_2 = new JSONObject(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d(TAG,"JSONException",e);
+
+            Toast.makeText(this,"Error creando el objeto JSON",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest sr = new JsonObjectRequest(
+                Request.Method.PUT,
+                "http://10.0.2.2:8080/myapp/PistonApp/escuderias/"+id,
+                js_2,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d(TAG, "" + response.toString());
+
+                        Toast.makeText(getApplicationContext(), 	"escuderia creada", Toast.LENGTH_LONG).show();
+                        finish();
+
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Error.ResponseREST " + error.networkResponse.statusCode);
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data,
+                                HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        // Now you can use any deserializer to make sense of data
+                        JSONObject obj = new JSONObject(res);
+                        Log.d(TAG, "Error.ResponseREST A: " + obj.toString());
+                    } catch (UnsupportedEncodingException e1) {
+                        // Couldn't properly decode data to string
+                        e1.printStackTrace();
+                        Log.d(TAG, "Error.ResponseREST B: " + e1.toString());
+                    } catch (JSONException e2) {
+                        // returned data is not JSONObject?
+                        e2.printStackTrace();
+                        Log.d(TAG, "Error.ResponseREST C: " + e2.toString());
+                    }
+                }
+            }
+        }){
+
+            /*
+            //  Esto (getParams) no lo estoy usando como tal pero entiendo que sirve para mapear los
+            //  parametros según el tipo de dato.
+            */
+
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+
+                return params;
+            }
+
+            /*
+            //  Esto (getHeaders) da condiciones a la solicitud con el encabezado de http.
+            //  Como el servidor consume JSON, especifico que ese es el tipo de contenido que
+            //  voy a utilizar. Y utf-8 porque... eso decía internet jaja.. Supongo que es lo
+            //  mas estándar y hace que cosas mas de ASCII no molesten
+            */
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+            /*@Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                int mStatusCode= response.statusCode;
+                Log.d(TAG,"STATUS CODE: "+mStatusCode);
+                return super.parseNetworkResponse(response.);
+            }*/
+        };
+
+        /*
+        //  Agrego lo que armé para hacer la petición con Volley
+        */
+        Volley.newRequestQueue(this).add(sr);
+        //queue.add(sr);
+
+    }
+
+    public void getListaEscuderias(){
+        if(!escuderias.isEmpty()){
+            escuderias.clear();
+        }
+        RequestQueue queue = Volley.newRequestQueue(ActualizarEscuderiaView.this);
+        String url = "http://10.0.2.2:8080/myapp/PistonApp/";
+        String path = "escuderias";
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url+path, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        try {
+
+                            for (int a = 0; a < jsonArray.length(); a++) {
+                                JSONObject obj = jsonArray.getJSONObject(a);
+                                Escuderia e = new Escuderia(obj.getString("nombre"),
+                                        obj.getString("lugarBase"),
+                                        obj.getString("jefeTecnico"),
+                                        obj.getString("jefeEquipo"),
+                                        obj.getString("chasis"),
+                                        Integer.parseInt(obj.getString("cant_vecesEnPodio")),
+                                        Integer.parseInt(obj.getString("cant_TitulosCampeonato")),
+                                        obj.getString("fotoEscudo_ref"));
+                                escuderias.add(e);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG, "Error handling rest invocation"+error.getCause());
+                    }
+                }
+        );
+        queue.add(req);
     }
 
 }
